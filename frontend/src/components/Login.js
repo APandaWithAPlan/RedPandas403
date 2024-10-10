893108dd5fa8f0e4f49d108289db0cfb41d09e98
ljk005 <logan.j.knies@gmail.com>
2024-10-09 21:39:40 -0500

    New React Ui for Hoempage,signup,login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,22 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
 function Login() {
-    return (
-      <div>
-        <h1>Login</h1>
-        <form>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Login;
-  
-  
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/signup" style={{ color: '#22d81b' }}>Register here!</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,25 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Signup.css';
+
 function Signup() {
-    return (
-      <div>
+  return (
+    <div className='background'>
+      <div className="signup">
         <h1>Signup</h1>
         <form>
           <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required />
+            <label htmlFor="First Name">First Name:</label>
+            <input type="text" id="FirstName" name="FirstName" required />
+          </div>
+          <div>
+            <label htmlFor="Last Name">Last Name:</label>
+            <input type="text" id="LastName" name="LastName" required />
           </div>
           <div>
             <label htmlFor="studentEmail">Student Email:</label>
             <input type="email" id="studentEmail" name="studentEmail" required />
           </div>
           <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div>
             <label htmlFor="newPassword">New Password:</label>
             <input type="password" id="newPassword" name="newPassword" required />
           </div>
+          <div>
+            <label htmlFor="Re EnterPassword">Re-enter Password</label>
+            <input type="password" id="Re-enterPassword" name="Re-enterPassword" required />
+          </div>
           <button type="submit">Signup</button>
         </form>
+        <p>
+          Already have an account? <Link to="/login" style={{ color: '#22d81b' }}>Login</Link>
+        </p>
       </div>
-    );
-  }
-  
-  export default Signup;
-  
+    </div>
+  );
+}
+
+export default Signup;
