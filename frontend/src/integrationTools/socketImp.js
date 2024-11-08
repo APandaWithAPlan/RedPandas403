af1f9382cfbf02a836aded1fe9f2818c55a3fa79
brilannebays <127992870+brilannebays@users.noreply.github.com>
2024-11-08 02:49:19 -0600

    Update socketImp.js
    Created frontend connection to the socket server

--- frontend/src/integrationTools/socketImp.js
+++ frontend/src/integrationTools/socketImp.js
@@ -1,1 +1,19 @@
+import {io} from 'socket.io-client';
+
+let socket;
+const socketConnection = username => {
+    if (socket && socket.connected) {
+        return socket;
+    } else {
+        socket = io.connect('https://localhost:3000/', {
+            auth: {
+                username: "bbays2024",
+                password: "softwaredesign",
+            }
+        });
+        return socket;
+    }
+}
+
+export default socketConnection;
 
