8ad22c1fea6d1ff90c1f2db094d29d28f9476b6e
brilannebays <127992870+brilannebays@users.noreply.github.com>
2024-11-12 22:08:10 -0600

    Update emitAnswers.js

--- frontend/src/components/emitAnswers.js
+++ frontend/src/components/emitAnswers.js
@@ -1,3 +1,27 @@
+const clientSocketListeners = (socket, typeOfCall, callStatus, updateCallStatus,peerConnection)=>{
+    socket.on('answerResponse',entireOfferObj=>{
+        console.log(entireOfferObj);
+        const copyCallStatus = {...callStatus}
+        copyCallStatus.answer = entireOfferObj.answer
+        copyCallStatus.myRole = typeOfCall
+        updateCallStatus(copyCallStatus)
+    })
+
+    // we got an ice candidate, let's add it to peerConnection
+    socket.on('receivedIceCandidateFromServer',iceC=>{
+        if(iceC){
+            peerConnection.addIceCandidate(iceC).catch(err=>{
+                console.log("Chrome thinks there is an error. There isn't...")
+            })
+            console.log(iceC)
+            console.log("Added an iceCandidate to existing page presence")
+            // setShowCallInfo(false);
+        }
+    })
+}
+
+export { clientSocketListeners };
+
 /*
 This code was transferred directly into Call.js
 
